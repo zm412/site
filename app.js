81eb02441fa563408841602d36dcd89e94a9ed3b
zm412 <zm412412@gmail.com>
2022-05-17 16:03:17 -0400

    test2

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const cors = require("cors");
 const express = require("express"),
   app = express(),
   http = require("http").createServer(app);
+/*
 var corsOptions = {
   origin: "http://127.0.0.1:8000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
+*/
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(bodyParser.json());
-app.set("view engine", "html");
 app.set("views", path.join(__dirname, "dist"));
 app.engine("html", require("hbs").__express);
+app.set("view engine", "html");
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/new_finst/", (req, res) => res.render("new_form_inst"));
