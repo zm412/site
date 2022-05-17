c68040e121112418ff5f543625ac0db319a48855
zm412 <zm412412@gmail.com>
2022-05-17 17:41:46 -0400

    test3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.engine("html", require("hbs").__express);
 app.set("view engine", "html");
 
 app.get("/", (req, res) => res.render("index"));
-app.get("/new_finst/", (req, res) => res.render("new_form_inst"));
+//app.get("/new_finst/", (req, res) => res.render("new_form_inst"));
 
 app.listen(process.env.PORT || 5000, () =>
   console.log("Server is running, localhost:5000")
